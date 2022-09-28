509bd283ec2637d9183e5d5bb30666829aa85ce5
mominur-helios <mominur.rahman@heliosinsider.com>
2022-09-28 15:35:53 +0600

    addeadded dashboard templates

--- admin_material/apps.py
+++ admin_material/apps.py
@@ -2,3 +2,10 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+
+from django.apps import AppConfig
+
+class AdminMaterialDashboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_material'
+    icon = 'fa fa-user'
